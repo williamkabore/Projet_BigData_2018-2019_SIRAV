d171f682f868251edae4a0cf9336d30bad5ebb70
williamkabore <39778870+williamkabore@users.noreply.github.com>
2019-02-15 14:55:59 +0100

    Update partie2.java

--- src/main/java/com/bigdata/projet/partie2.java
+++ src/main/java/com/bigdata/projet/partie2.java
@@ -16,8 +16,8 @@ public class partie2 {
 		return selected;
 
 	}
-	/*La fonction GroupConnex, prend en parametres 3 colones et un string, elle crée une nouvelle colones appelée
-	* Connex, qui contiendra une pair constitué des deux colonnes col2 et col3
+	/*La fonction GroupConnex, prend en paramètres 3 colonnes et un string, elle crée une nouvelle colones appelée
+	* Connex, qui contiendra une paire constituée des deux colonnes col2 et col3
 	* Le resultat de retour est un dataset
 	*/
 	public static Dataset<Row> GroupConnex (Dataset<Row> ds, String col1, String col2, String col3,String Connex) {
@@ -28,7 +28,7 @@ public class partie2 {
 		return Grouped;
 
 	}
-	/*La fonction ColUnion, utilise la methode Union pour fusioner les deux colones du dataset precedent.
+	/*La fonction ColUnion, utilise la methode Union pour fusioner les deux colones du dataset précédent.
 	*/
 	public static Dataset<Row> ColUnion (Dataset<Row> ds, String col1, String col2, String name) {
 	    Dataset<Row> Union = ds.select(col1)
