598203ebc038115d46147cb0ed69c1c15fa3c70d
Aziz BEKKI <azizgstest@gmail.com>
2019-02-15 11:16:56 +0100

    Update partie1.java

--- src/main/java/com/bigdata/projet/partie1.java
+++ src/main/java/com/bigdata/projet/partie1.java
@@ -1,3 +1,9 @@
+/*
+* La classe Partie contient des fonctions permettant de traiter les questions relatives à la partie 1 du projet.
+*
+*/
+
+
 package com.bigdata.projet;
 
 import org.apache.spark.sql.Dataset;
@@ -6,9 +12,9 @@ import org.apache.spark.sql.SparkSession;
 
 public class partie1  {
 
-
-
-
+/* La fonction newSession permet de creer une nouvelle session Spark et definit les parametres d'environnement 
+* de l'application. Le resultat de retour est une session. 
+*/
 	public static SparkSession newSession () {
 		SparkSession Spark = SparkSession.builder().master("local[*]")
 				.appName("BEKKI_KABORE_APP")
@@ -16,7 +22,9 @@ public class partie1  {
 				.getOrCreate();
 		return Spark; 
 	}
-
+/* La fonction readfile permet de lire un fichier et le formater en dataset. Elle retourne un dataset the type Row.  
+*/
+	
 	public static Dataset<Row> readfile(String File_name, SparkSession spark) {
 		Dataset<Row> dataset = spark.read()
 				.option("delimiter", ",")
